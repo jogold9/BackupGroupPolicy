6c9f59af3ab67673051db63af83cae2566e55631
Josh <joshuabgold@gmail.com>
2018-09-11 22:07:19 -0700

    Update GroupPolicyBackup.ps1
    Created new variable for path name, changed order of code to be more organized and logical

--- GroupPolicyBackup.ps1
+++ GroupPolicyBackup.ps1
@@ -1,5 +1,5 @@
 #Josh Gold
-#This Powershell script backs up all Microsoft Group Policy Objects for a single domain and deletes backups older than 60 days
+#This Powershell script backs up all Microsoft Group Policy Objects for a single domain and deletes backups older than 45 days
 
 #Use these two lines if you want to force the script to follow Powershell best practices
 Set-StrictMode -Version Latest
@@ -10,16 +10,16 @@ $ErrorActionPreference = 'Stop'
 
 Import-Module GroupPolicy
 
-#Backs up all Group Policy Objects to specificed path
-Get-GPO -All | foreach { Backup-GPO -Name $_.DisplayName -Path "D:\Depts\ITS_Fldr\Group Policy\Backups" -Comment $_.DisplayName }
+#Path to the Root Folder
+$PathName = "D:\SomeFolder\IT\Group Policy\Backups"
 
-#Delete folders and files older than 60 days
+#Backs up all Group Policy Objects to specificed path
+Get-GPO -All | foreach { Backup-GPO -Name $_.DisplayName -Path $PathName -Comment $_.DisplayName }
 
 # Enter a number to indicate how many days old the identified file needs to be (must have a "-" in front of it).
-$HowOld = -60
+$HowOld = -45
 
-#Path to the Root Folder
-$Path = "D:\Depts\ITS_Fldr\Group Policy\Backups"
+#Delete folders and files older than 45 days
+#Remove the "-WhatIf" at the end, else the script will only show the files rather than delete them.
 
-# Remove the "-WhatIf" at the end, else the script will only show the files rather than delete them.
-get-childitem $Path -recurse | where {$_.lastwritetime -lt (get-date).adddays($HowOld) -and -not $_.psiscontainer} |% {remove-item $_.fullname -force}
+get-childitem $PathName -recurse | where {$_.lastwritetime -lt (get-date).adddays($HowOld) -and -not $_.psiscontainer} |% {remove-item $_.fullname -force}
